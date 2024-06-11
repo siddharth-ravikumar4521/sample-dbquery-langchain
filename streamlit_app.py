26275cb428b54360a99f8d4311273af097b4a76b
siddharth-ravikumar4521 <siddharth.ravikumar@aaseya.com>
2024-06-11 18:40:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,23 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import os
+from openai import OpenAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.agents.agent_types import AgentType
+from langchain_community.utilities import SQLDatabase
+import sqlite3
+from langchain.chains import create_sql_query_chain
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+from operator import itemgetter
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
+
 
 """
 # Welcome to Streamlit!
@@ -13,28 +30,101 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# OpenAI API key
+open_ai_api = "sk-proj-N7m3Hr6mhKZe8BzdhMQST3BlbkFJiw5H8yFX2bmP3WnhStzK"
+os.environ["OPENAI_API_KEY"] = open_ai_api
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# Streamlit application layout
+st.title("Momrah Butler")
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+os.environ["OPENAI_API_KEY"] = open_ai_api
+db = SQLDatabase.from_uri("sqlite:///your_database.db")
+#print(db.dialect)
+#print(db.get_usable_table_names())
+st.write(f"{db.get_usable_table_names()}")
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+execute_query = QuerySQLDataBaseTool(db=db)
+write_query = create_sql_query_chain(llm, db)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
+
+Question: {question}
+SQL Query: {query}
+SQL Result: {result}
+Answer: """
+)
+
+answer = answer_prompt | llm | StrOutputParser()
+chain = (
+    RunnablePassthrough.assign(query=write_query).assign(
+        result=itemgetter("query") | execute_query
+    )
+    | answer
+)
+
+client = OpenAI(api_key=open_ai_api)
+
+def translate_text(text):
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"Translate the following text to English: {text}",
+                }
+            ],
+            model="gpt-3.5-turbo"
+        )
+        translation = response.choices[0].message.content.strip()
+        return translation
+
+def translate_text_arabic(text):
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"Translate the following text to Arabic: {text}",
+                }
+            ],
+            model="gpt-3.5-turbo"
+        )
+        translation = response.choices[0].message.content.strip()
+        return translation
+
+arabic_text = st.text_input("Enter some text:")
+eng_prompt=translate_text(arabic_text)
+response=chain.invoke({"question": eng_prompt})
+arabic_response=translate_text_arabic(response)
+
+st.write(f"{response}")
+st.write("\n\n")
+st.write(f"{arabic_response}")
+#st.write(f"Text entered: {text_input}")
+
+
+#num_points = st.slider("Number of points in alliswell123", 1, 10000, 1100)
+#num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+
+#indices = np.linspace(0, 1, num_points)
+#theta = 2 * np.pi * num_turns * indices
+#radius = indices
+
+#x = radius * np.cos(theta)
+#y = radius * np.sin(theta)
+
+#df = pd.DataFrame({
+#    "x": x,
+#    "y": y,
+#    "idx": indices,
+#    "rand": np.random.randn(num_points),
+#})
+
+#st.altair_chart(alt.Chart(df, height=700, width=700)
+#    .mark_point(filled=True)
+#    .encode(
+#        x=alt.X("x", axis=None),
+#        y=alt.Y("y", axis=None),
+#        color=alt.Color("idx", legend=None, scale=alt.Scale()),
+#        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+ #   ))
